efea249e3f2b4cb58bd525a9585cc34bf6e4cacd
tmtuan <tmtuanyd@gmail.com>
2021-04-11 21:46:06 +0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,31 @@ import React from "react";
 import BlogPostContainer from "./BlogPostContainer";
 import Header from "./Header";
 import BlogDetail from "./BlogDetail";
+import axios from "axios";
 
-function App() {
-  return (
-      <div>
-          <Header/>
-          <Switch>
-              <Route path={"/login"} component={LoginForm} />
-              <Route path={"/"} component={BlogPostContainer} />
-              <Route path={"/blog-post/:id"} component={BlogDetail} />
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        const token = window.localStorage.getItem('jwtToken')
+        if(token){
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+        }
+    }
 
-          </Switch>
-      </div>
+    render(){
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route path={"/login"} component={LoginForm} />
+                    <Route path={"/"} component={BlogPostContainer} />
+                    <Route path={"/blog-post/:id"} component={BlogDetail} />
 
-  );
+                </Switch>
+            </div>
+
+        );
+    }
 }
 
 export default App;
